dd64cf11bd2378145e45387ddf4301062a86d8c0
trezeguit <mahmoudhassan7764@outlook.com>
2020-09-19 00:12:24 +0200

    Updates

---
+++ precache-manifest.48100bb8f23b5468490295dd07b61372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd53c3c50ed0a8faeedb13fc42e4b3b",
+    "url": "/mahmoud-hassan-cv/index.html"
+  },
+  {
+    "revision": "9ea1ee5a4d1015b9d4cc",
+    "url": "/mahmoud-hassan-cv/static/css/main.d1448d77.chunk.css"
+  },
+  {
+    "revision": "8191e4350a132db2dffb",
+    "url": "/mahmoud-hassan-cv/static/js/2.3558a9fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mahmoud-hassan-cv/static/js/2.3558a9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea1ee5a4d1015b9d4cc",
+    "url": "/mahmoud-hassan-cv/static/js/main.15bf6913.chunk.js"
+  },
+  {
+    "revision": "d0404c345efff1d3ffbe",
+    "url": "/mahmoud-hassan-cv/static/js/runtime-main.e3320d2e.js"
+  }
+]);
